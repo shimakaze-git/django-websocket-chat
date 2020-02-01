fa6efc4a5a1d32880b50d328dd4a1b7e4dbcd6d7
shimakaze-git <shimakaze.soft+github@googlemail.com>
2020-02-01 20:07:08 +0900

    fix: STATICFILES_DIRSを作成.

--- django_websocket_chat/chats/urls.py
+++ django_websocket_chat/chats/urls.py
@@ -2,15 +2,14 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-
     path(
         "",
         TemplateView.as_view(template_name="room/index.html"),
-        # name="user_mypage",
+        name="room_list"
     ),  # Room部屋
-    # path(
-        # "<room_id>/",
-        # TemplateView.as_view(template_name="content.html"),
-        # name="user_page_config",
-    # ),  # Room部屋
+    path(
+        "<room_id>/",
+        TemplateView.as_view(template_name="room/page.html"),
+        name="room_page"
+    ),  # Room部屋
 ]
--- django_websocket_chat/config/settings.py
+++ django_websocket_chat/config/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 LOGGING = {
     'version': 1,  # これを設定しないと怒られる
